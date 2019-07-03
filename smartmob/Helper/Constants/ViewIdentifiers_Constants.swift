e10cc8de5ec9c1bdee5f3519ea8aac0bfc81168c
Santosh Kc <santosh@artup.io>
2019-07-03 17:25:04 +0545

    Added Viewidentifiers in structs

---
+++ smartmob/Helper/Constants/ViewIdentifiers_Constants.swift
@@ -0,0 +1,18 @@
+//
+//  ViewIdentifiers_Constants.swift
+//  smartmob
+//
+//  Created by santosh kc on 7/3/19.
+//  Copyright © 2019 Santosh Kc. All rights reserved.
+//
+
+import Foundation
+
+struct ViewIdentifier {
+    static var photoListViewController: String { return "PhotoListViewController" }
+    static var photoDetailViewController: String { return "PhotoDetailViewController" }
+}
+
+struct Storyboard {
+    static var main: String { return "Main" }
+}
