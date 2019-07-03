07905829909f5efd5b8eeb502832289535194c55
Santosh Kc <santosh@artup.io>
2019-07-03 12:02:06 +0545

    added Api struct to store urls

---
+++ smartmob/Helper/Constants/Network+Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Network+Constants.swift
+//  smartmob
+//
+//  Created by santosh kc on 7/3/19.
+//  Copyright © 2019 Santosh Kc. All rights reserved.
+//
+
+import Foundation
+
+struct Api {
+    static var latestAssestsURL: String { return "http://www.splashbase.co/api/v1/images/latest"}
+    static var searchQueryURL: String { return "http://www.splashbase.co/api/v1/images/search"}
+}
+
