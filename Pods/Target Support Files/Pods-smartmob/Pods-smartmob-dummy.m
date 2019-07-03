ef16daec12bdfa26c1fd6937bd0161052ab4bd93
Santosh Kc <santosh@artup.io>
2019-07-03 11:19:01 +0545

    Project setup
    Created podfile and workspace

---
+++ Pods/Target Support Files/Pods-smartmob/Pods-smartmob-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_smartmob : NSObject
+@end
+@implementation PodsDummy_Pods_smartmob
+@end
---
+++ Pods/Target Support Files/Pods-smartmobTests/Pods-smartmobTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_smartmobTests : NSObject
+@end
+@implementation PodsDummy_Pods_smartmobTests
+@end
