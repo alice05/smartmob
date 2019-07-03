dc7c040553d068643a266e546ecf803e6c2db46a
Santosh Kc <santosh@artup.io>
2019-07-03 17:30:29 +0545

    Added photodetailviewcontroller

---
+++ smartmob/UI/PhotoDetailView/PhotoDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoDetailViewController.swift
+//  smartmob
+//
+//  Created by santosh kc on 7/3/19.
+//  Copyright © 2019 Santosh Kc. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
