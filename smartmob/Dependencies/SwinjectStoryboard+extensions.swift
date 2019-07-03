adb09fc208d820d979de3a366c86e6224c435c35
Santosh Kc <santosh@artup.io>
2019-07-03 17:43:40 +0545

    Added extension of swinject storyboard

---
+++ smartmob/Dependencies/SwinjectStoryboard+extensions.swift
@@ -0,0 +1,24 @@
+//
+//  SwinjectStoryboard+extensions.swift
+//  smartmob
+//
+//  Created by santosh kc on 7/3/19.
+//  Copyright © 2019 Santosh Kc. All rights reserved.
+//
+
+import SwinjectStoryboard
+
+extension SwinjectStoryboard {
+    
+    public static func setup() {
+        if AppDelegate.dependencyRegistry == nil {
+            AppDelegate.dependencyRegistry = DependencyRegistryImpl(container: defaultContainer)
+        }
+        
+        let dependencyRegistry: DependencyRegistry = AppDelegate.dependencyRegistry
+        
+        dependencyRegistry.container.storyboardInitCompleted(PhotoListViewController.self) { (resolver, vc) in
+            
+        }
+    }
+}
